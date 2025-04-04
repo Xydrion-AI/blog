0860e2b4eb1814a7689fcbaff7b6a1558189253b
Xydrion-AI <davidpetiot35@gmail.com>
2025-04-04 14:37:05 +0200

    Unit Test Uri

---
+++ public/index.php
@@ -0,0 +1,9 @@
+<?php
+
+use GuzzleHttp\Psr7\ServerRequest;
+
+require '../vendor/autoload.php';
+
+$app = new \Framework\App();
+
+$response = $app->run(ServerRequest::fromGlobals());
---
+++ tests/Framework/AppTest.php
@@ -0,0 +1,18 @@
+<?php
+namespace Tests\Framework;
+
+
+use Framework\App;
+use GuzzleHttp\Psr7\ServerRequest;
+use PHPUnit\Framework\TestCase;
+
+class AppTest extends TestCase {
+    public function test404() {
+        $app = new App();
+        $request = new ServerRequest('GET', '/404/'); //Need to install a new library https://packagist.org/ -> https://packagist.org/packages/guzzlehttp/psr7 and in terminal, write : composer require guzzlehttp/psr7 to install guzzlehttp/psr7
+        /*$_SERVER['REQUEST_URI'] = '/estelle/'; Why this part was written and deleted ?!*/
+        $response = $app->run($request);
+        $this->assertStringContainsString('<h1>Error 404</h1>', (string)$response->getBody());
+        $this->assertEquals(404, $response->getStatusCode());
+    }
+}
