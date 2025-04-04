db2fa0b083518603ea1cf6210e454c7396552326
Xydrion-AI <davidpetiot35@gmail.com>
2025-04-04 14:37:50 +0200

    .

--- tests/Framework/AppTest.php
+++ tests/Framework/AppTest.php
@@ -6,8 +6,10 @@ use Framework\App;
 use GuzzleHttp\Psr7\ServerRequest;
 use PHPUnit\Framework\TestCase;
 
-class AppTest extends TestCase {
-    public function test404() {
+class AppTest extends TestCase
+{
+    public function test404() 
+    {
         $app = new App();
         $request = new ServerRequest('GET', '/404/'); //Need to install a new library https://packagist.org/ -> https://packagist.org/packages/guzzlehttp/psr7 and in terminal, write : composer require guzzlehttp/psr7 to install guzzlehttp/psr7
         /*$_SERVER['REQUEST_URI'] = '/estelle/'; Why this part was written and deleted ?!*/
